d89e85cd25c60ae06195f712bc9cb6b47688511f
Vanshika Gupta <vanshikagupta0009@gmail.com>
2024-07-04 19:50:34 +0530

    commit

--- app/src/main/java/com/vanshika/recyclerviewapplication/MainActivity.kt
+++ app/src/main/java/com/vanshika/recyclerviewapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.vanshika.recyclerviewapplication
 
+import android.app.AlertDialog
 import android.app.Dialog
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
@@ -12,16 +13,18 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.vanshika.recyclerviewapplication.databinding.ActivityMainBinding
 import com.vanshika.recyclerviewapplication.databinding.CustomDialogBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TaskClickInterface {
     var binding : ActivityMainBinding ?= null
     lateinit var manager : LinearLayoutManager
     var list = arrayListOf<TaskDataClass>()
-    var adapter : TaskRecyclerAdapter = TaskRecyclerAdapter(list)
+    var adapter : TaskRecyclerAdapter = TaskRecyclerAdapter(this,list,this)
+    lateinit var toDoDatabase : TodoDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-        manager = LinearLayoutManager(this)
+        toDoDatabase = TodoDatabase.getInstance(this)
+        manager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
         binding?.recyclerView?.layoutManager = manager
         binding?.recyclerView?.adapter = adapter
         binding?.fab?.setOnClickListener {
@@ -39,22 +42,93 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, resources.getString(R.string.select_one), Toast.LENGTH_SHORT).show()
                 }
                 else{
+                    var priority = if (dialogBinding.rbLow.isChecked)
+                        1
+                    else if (dialogBinding.rbMedium.isChecked)
+                        2
+                    else if (dialogBinding.rbHigh.isChecked)
+                        3
+                    else
+                        0
                     list.add(TaskDataClass(
-                        dialogBinding.etTitle.text.toString(),
-                        dialogBinding.etDescription.text.toString()
+                        title = dialogBinding.etTitle.text.toString(),
+                        description = dialogBinding.etDescription.text.toString(),
+                        priority = priority
                     ))
-                    dialogBinding.rbLow.setOnClickListener {
-                        binding?.recyclerView?.setBackgroundColor(resources.getColor(R.color.orange))
-                    }
-                    dialogBinding.rbMedium.setOnClickListener {
-                        binding?.recyclerView?.setBackgroundColor(resources.getColor(R.color.blue))
-                    }
-                    dialogBinding.rbHigh.setOnClickListener {
-                        binding?.recyclerView?.setBackgroundColor(resources.getColor(R.color.green))
-                    }
+//                    toDoDatabase.todoDao().insertTodo(
+//                        TaskDataClass(
+//                            title = dialogBinding.etTitle.text.toString(),
+//                            description = dialogBinding.etDescription.text.toString(),
+//                            priority = priority
+//                        )
+//                    )
+//                    getList()
                     dialog.dismiss()
                 }
             }
         }
+//        getList()
+    }
+
+    override fun updateTask(position: Int) {
+        Dialog(this).apply {
+            var dialogBinding = CustomDialogBinding.inflate(layoutInflater)
+            setContentView(dialogBinding.root)
+            window?.setLayout(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            show()
+            dialogBinding.btnAdd.setOnClickListener {
+                if (dialogBinding.etTitle.text.toString().isEmpty()){
+                    dialogBinding.etTitle.error = resources.getString(R.string.enter_title)
+                }else if (dialogBinding.etDescription.text.toString().isEmpty()){
+                    dialogBinding.etDescription.error = resources.getString(R.string.enter_description)
+                }else if (dialogBinding.radioButton.checkedRadioButtonId == -1){
+                    Toast.makeText(this@MainActivity, resources.getString(R.string.select_one), Toast.LENGTH_SHORT).show()
+                }else {
+                    var priority = if (dialogBinding.rbLow.isChecked) {
+                        1
+                    } else if (dialogBinding.rbMedium.isChecked) {
+                        2
+                    } else if (dialogBinding.rbHigh.isChecked) {
+                        3
+                    } else {
+                        0
+                    }
+                    list.set(
+                        position, TaskDataClass(
+                            title = dialogBinding.etTitle.text.toString(),
+                            description = dialogBinding.etDescription.text.toString(),
+                            priority = priority
+                        )
+                    )
+//                    toDoDatabase.todoDao().updateList(TaskDataClass(
+//                        id = 0,
+//                        title = dialogBinding.etTitle.text.toString(),
+//                        description = dialogBinding.etDescription.text.toString(),
+//                        priority = priority
+//                    ))
+                    adapter.notifyDataSetChanged()
+                    dismiss()
+                }
+            }
+        }
+    }
+
+    override fun deleteTask(position: Int) {
+        var alertDialog = AlertDialog.Builder(this)
+        alertDialog.setTitle(resources.getString(R.string.are_you_sure_you_want_to_delete_this))
+        alertDialog.setPositiveButton(resources.getString(R.string.yes)){_,_ ->
+            list.removeAt(position)
+            adapter.notifyDataSetChanged()
+        }
+        alertDialog.setNegativeButton(resources.getString(R.string.no)){_,_ ->
+        }
+        alertDialog.show()
+    }
+    fun getList(){
+        list.addAll(toDoDatabase.todoDao().getList())
+        adapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/vanshika/recyclerviewapplication/TaskRecyclerAdapter.kt
+++ app/src/main/java/com/vanshika/recyclerviewapplication/TaskRecyclerAdapter.kt
@@ -1,18 +1,23 @@
 package com.vanshika.recyclerviewapplication
 
+import android.content.Context
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.CompoundButton
 import android.widget.RadioButton
+import androidx.core.content.ContextCompat
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class TaskRecyclerAdapter(var list : ArrayList<TaskDataClass>) : RecyclerView.Adapter<TaskRecyclerAdapter.ViewHolder>() {
+class TaskRecyclerAdapter(var context : Context, var list : ArrayList<TaskDataClass>, var taskClickInterface: TaskClickInterface) : RecyclerView.Adapter<TaskRecyclerAdapter.ViewHolder>() {
     class ViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
         var tvTitle :TextView = view.findViewById(R.id.tvTitle)
         var tvDescription : TextView = view.findViewById(R.id.tvDescription)
+        var btnUpdate : Button = view.findViewById(R.id.btnUpdate)
+        var btnDelete : Button = view.findViewById(R.id.btnDelete)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -27,5 +32,19 @@ class TaskRecyclerAdapter(var list : ArrayList<TaskDataClass>) : RecyclerView.Ad
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.tvTitle.setText(list[position].title)
         holder.tvDescription.setText(list[position].description)
+        when(list[position].priority){
+            1->
+                holder.itemView.setBackgroundColor(ContextCompat.getColor(context,R.color.orange))
+            2->
+                holder.itemView.setBackgroundColor(ContextCompat.getColor(context,R.color.blue))
+            3->
+                holder.itemView.setBackgroundColor(ContextCompat.getColor(context,R.color.green))
+        }
+        holder.btnUpdate.setOnClickListener {
+            taskClickInterface.updateTask(position)
+        }
+        holder.btnDelete.setOnClickListener {
+            taskClickInterface.deleteTask(position)
+        }
     }
 }
